2d9225739672913d17a73ce513df3d991cfb36bb
fresky <freskytobing69@gmail.com>
2023-12-11 17:15:42 +0700

    test ui jemaat,pindah

--- PPL_Test/test/specs/admin/appjemaat.js
+++ PPL_Test/test/specs/admin/appjemaat.js
@@ -15,21 +15,5 @@ describe('Laravel Project Testing', () => {
         // Navigate to the target page
         await browser.url('http://127.0.0.1:8000/admin/dashboard');
 
-        // Click on the "Approve" dropdown
-        const approveDropdown = await $('a.nav-link=i\\[src\\="/icon/aproval.png"\\]');
-        await approveDropdown.click();
-
-        // Click on the "Jemaat Pindah" link
-        const jemaatPindahLink = await $('a.nav-link[p>Jemaat Pindah]');
-        await jemaatPindahLink.click();
-
-        // Find and click the "Approve" button for the specified Jemaat (Parso)
-        const jemaatNameCell = await $('td.dtr-control.sorting_1');
-        await jemaatNameCell.click();
-
-        const approveButton = await $('a.btn.btn-success[data-toggle="tooltip"][title="uprove"]');
-        await approveButton.click();
-
-        // You might want to add some assertions here to verify the result if needed
     });
 });
